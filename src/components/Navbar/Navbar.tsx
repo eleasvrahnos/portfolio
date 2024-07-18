755990cdb543432d6cd0da67860e9cc7a22490a3
Eleas Vrahnos <evrahnos1966@gmail.com>
2024-07-17 20:14:08 -0700

    made website and fixed navbar on devices

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="bg-richblack p-6">
       <div>
-        <ul className="flex justify-center space-x-6 text-sm font-medium text-gray-300 sm:space-x-14 sm:text-2xl">
+        <ul className="flex justify-center gap-x-[5%] text-sm font-medium text-gray-300 sm:text-2xl">
           <li className="duration-200 hover:scale-125">
             <a href="#about">About</a>
           </li>
