a3ced56e70c4e70082a49f177c9095c96133676b
eleasvrahnos <evrahnos1966@gmail.com>
2025-02-03 22:32:39 -0800

    compressed project videos

--- src/assets/experience/experienceIcons.ts
+++ src/assets/experience/experienceIcons.ts
@@ -1,6 +1,5 @@
 import codingminds from "./icons/codingminds.jpg";
 import headstarter from "./icons/headstarter.jpg";
 import turionspace from "./icons/turionspace.png";
-import tutor from "./icons/tutor.png";
 
-export { codingminds, headstarter, turionspace, tutor };
+export { codingminds, headstarter, turionspace };
